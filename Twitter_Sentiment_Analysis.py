ec8e3ba54f77e173d764bdd4709fb4345c030428
Justin John <46942606+jjohn50@users.noreply.github.com>
2020-12-15 15:59:13 -0500

    Add files via upload
    Last Push. All the features for the product are done.

--- Twitter_Sentiment_Analysis.py
+++ Twitter_Sentiment_Analysis.py
@@ -1,17 +1,16 @@
 #Going to need the following libraries: 
 #tweepy, textblob, and some NLTK stuff 
-#Used this website to help get started: 
-# https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
-# What I can do is use machine learning within this app that Geeks for Geeks helped with and than I can make sentiment analysis better
-# Use this link: https://monkeylearn.com/blog/sentiment-analysis-of-twitter/
+#Used this website to help get started: https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
 
 import re 
 import tweepy 
 from tweepy import OAuthHandler
 from textblob import TextBlob
 import matplotlib.pyplot as plt 
+import json
+import sys 
 
-class TwitterClient(object): 
+class SA(object): 
     ''' 
     Triple quotes are igonored in python
     Generic Twitter Class for sentiment analysis. 
@@ -22,10 +21,10 @@ class TwitterClient(object):
         For the sake of protection for this program, I will need to delete the consumer key, consumer secret, etc before uploading to github
         '''
         # keys and tokens from the Twitter Dev Console 
-        consumer_key = ''
-        consumer_secret = ''
-        access_token = ''
-        access_token_secret = ''
+        consumer_key = 'ebEvVT8o3cs3C21UBxKAZF9BJ'
+        consumer_secret = '1281ak3m45XuX9nYHuIDxMFRerVwrT1Lj4JXdOGJNVu1aYw71e'
+        access_token = '1336373971898560516-IPYXFdQUydWOB9rnjEIdeKLguTlTTA'
+        access_token_secret = 'qFU6BPvQi2kaoCL9pik8xjokAmvjSFrseAPeniNEsNHET'
   
         # standard attempt authentication 
         try: 
@@ -46,20 +45,19 @@ class TwitterClient(object):
   
     def get_tweet_sentiment(self, tweet): 
         ''' 
-        Utility function to classify sentiment of passed tweet 
-        using textblob's sentiment method 
+          Using textblob's sentiment method 
         '''
         # create TextBlob object of passed tweet text 
         analysis = TextBlob(self.clean_tweet(tweet)) 
-        # set sentiment 
+        # textblob will determine the sentiment of a tweet and we put it in as positive or negative
         if analysis.sentiment.polarity > 0: 
             return 'positive'
         elif analysis.sentiment.polarity == 0: 
             return 'neutral'
         else: 
             return 'negative'
-  
-    def get_tweets(self, query, count = 10): 
+                                #this controls how many tweets to get
+    def get_tweets(self, query, count = 2000): 
         ''' 
         Main function to fetch tweets and parse them. 
         '''
@@ -68,11 +66,11 @@ class TwitterClient(object):
   
         try: 
             # call twitter api to fetch tweets 
-            fetched_tweets = self.api.search(q = query, count = count) 
+            fetched_tweets = self.api.search(q = query, lang = "en", count = count) 
   
-            # parsing tweets one by one 
+            # pLooks at each tweet 
             for tweet in fetched_tweets: 
-                # empty dictionary to store required params of a tweet 
+                # Stores tweet here  
                 parsed_tweet = {} 
   
                 # saving text of tweet 
@@ -96,50 +94,105 @@ class TwitterClient(object):
             print("Error : " + str(e)) 
   
 def main(): 
-    # creating object of TwitterClient Class 
-    api = TwitterClient() 
-    # calling function to get tweets 
-    # Right now the code will manual need to look up queury term, but I can improve this by asking the user to put something in
-    search_term = input("Enter a term to look up on twitter and see how positive or negative it is ")
+    api = SA() 
+    search_term = input("Enter a term to look up ")
     print("_________________________________________________________________________________________________")
 
-    tweets = api.get_tweets(query = search_term, count = 200) 
+    tweets = api.get_tweets(query = search_term, count = 2000) 
   
     # picking positive tweets from tweets 
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
     # percentage of positive tweets 
-    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
+    print(str(search_term) + " Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
     # picking negative tweets from tweets 
     ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
     # percentage of negative tweets 
-    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
+    print(str(search_term) + " Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
     # percentage of neutral tweets 
-    print("Neutral tweets percentage: {} %".format(100*(len(tweets) -(len( ntweets )+len( ptweets)))/len(tweets))) 
-    #netweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral'] 
+    print(str(search_term) + " Neutral tweets percentage: {} %".format(100*(len(tweets) -(len( ntweets )+len( ptweets)))/len(tweets))) 
+    netweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral'] 
     # code for neural tweets, but it just gives garbage 
-    # printing first 5 positive tweets 
-    print("\n\nPositive tweets:") 
-    for tweet in ptweets[:100]: 
+
+    print(str(search_term) + "\n\nPositive tweets:") 
+    print("__________________________________________")
+    for tweet in ptweets[:360]: 
         print(tweet['text'])       
   
-    # printing first 5 negative tweets 
-    print("\n\nNegative tweets:") 
-    for tweet in ntweets[:100]: 
+    print(str(search_term) + "\n\nNegative tweets:") 
+    print("__________________________________________")
+    for tweet in ntweets[:360]: 
         print(tweet['text']) 
 
+    print(str(search_term) + "\n\nNeutral  tweets:") 
+    print("__________________________________________")
+    for tweet in netweets[:360]: 
+        print(tweet['text']) 
+
+    #see if the item you looked up is within trending 
+    consumer_key = 'ebEvVT8o3cs3C21UBxKAZF9BJ'
+    consumer_secret = '1281ak3m45XuX9nYHuIDxMFRerVwrT1Lj4JXdOGJNVu1aYw71e'
+    access_token = '1336373971898560516-IPYXFdQUydWOB9rnjEIdeKLguTlTTA'
+    access_token_secret = 'qFU6BPvQi2kaoCL9pik8xjokAmvjSFrseAPeniNEsNHET'
     
-    #I can use most of this code since its basis stuff that out there, but I can expaned upon it with the use of graphics and charts
+    # authorization of consumer key and consumer secret 
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+    
+    # set access to user's access key and access secret  
+    auth.set_access_token(access_token, access_token_secret) 
+     
+    # calling the api  
+    api1 = tweepy.API(auth) 
+
+    #This part of the code will allow a user to see if their selected topic is trending in New York
+    print("Trending Topics Right Now in New York")
+    print("_____________________________________")
+    #After doing some research you need a Earth ID for special areas to get certain trending tweets
+    city = input("Choose one of the cities to see if you search term is trending : New York, Los Angeles, Toronto, London, Paris, or San Francisco ")
+
+    New_York_WOE_ID = 2459115
+    Los_Angeles_WOE_ID = 2442047
+    Toronto_WOE_ID = 4118
+    London_WOE_ID = 44418
+    Paris_WOE_ID = 615702
+    San_Francisco_WOE_ID = 2487956
+
+    if(city == "New York"):
+        Trending = api1.trends_place(New_York_WOE_ID)
+    elif(city == "Los Angeles"):
+        Trending = api1.trends_place(Los_Angeles_WOE_ID)
+    elif(city == "Toronto"):
+        Trending = api1.trends_place(Toronto_WOE_ID)
+    elif(city == "London"):
+        Trending = api1.trends_place(London_WOE_ID)
+    elif(city == "Paris"):
+        Trending = api1.trends_place(Paris_WOE_ID)   
+    elif(city == "San Francisco"):
+        Trending = api1.trends_place(San_Francisco_WOE_ID)
+    else:
+        print("Enter one of the following places")
+
+    # This code will just show trending topics in New York 
+    trends = json.loads(json.dumps(Trending, indent=1))
+    #The part above will parse through the data 
+    for trend in trends[0]["trends"]:
+        print((trend["name"]).strip("#"))
+
+    #This code will show trending topics if they match the search term
+    for trend in trends[0]["trends"]:
+        if(search_term == (trend["name"]).strip("#")):
+            print("The search term " + search_term + " is trending on Twitter right now ")   
+
     #Graph part: 
     #Got help from this website: https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
 
     #data for the graphs
 
     #x values  (pos, neg, neu, total)
-    type_of_tweets = ["Positive Tweets", "Negative Tweets", "Neutral Tweets"]
+    type_of_tweets = [str(search_term) + " Positive Tweets", str(search_term)+ " Negative Tweets", str(search_term) + "Neutral Tweets"]
     # y values 
     amount_tweets = len(ptweets), len(ntweets), (len(tweets) - (len(ptweets) + len(ntweets)))
 
-    tick_label = ["Positive Tweets", "Negative Tweets", "Neutral Tweets"]
+    tick_label = [str(search_term) + " Positive Tweets", str(search_term) + " Negative Tweets", str(search_term) + " Neutral Tweets"]
 
     #plotting of the graph
     plt.bar(type_of_tweets, amount_tweets, tick_label = tick_label, width = 0.8, color = ['Blue', 'Red', 'Gray'])
@@ -152,8 +205,6 @@ def main():
     plt.title("Tweets vs Type of Tweets")
     #displays the graph 
     plt.show()
-    
-if __name__ == "__main__": 
-    # calling main function 
-    main() 
 
+if __name__ == "__main__": 
+    main() 
